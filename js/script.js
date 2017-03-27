657044f317852847de5c5fb83e6eb1e2f603e71a
mari4w04 <mari4w04@stud.kea.dk>
2017-03-27 13:57:21 +0200

    Sound added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 let narrationSound1 = document.querySelector('#narration1');
+let midnightBtn = document.querySelector("#midnight-btn");
 
 narrationSound1.play();
 
 narrationSound1.addEventListener('ended', show1stButton);
+
+function show1stButton(){
+    midnightBtn.classList.add("show");
+};
