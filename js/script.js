92bfc442e29ada1a96257707b7a6b4e1375f1957
mari4w04 <mari4w04@stud.kea.dk>
2017-03-28 13:12:38 +0200

    Added buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let narrationSound1 = document.querySelector('#narration1');
 let midnightBtn = document.querySelector("#midnight-btn");
+let mainContainer = document.querySelector("#cont");
+let clockSound = document.querySelector("#clock-ring");
+let clockDiv = document.querySelector('#clock');
 
 narrationSound1.play();
 
@@ -8,3 +11,18 @@ narrationSound1.addEventListener('ended', show1stButton);
 function show1stButton(){
     midnightBtn.classList.add("show");
 };
+
+midnightBtn.addEventListener('click', showClock);
+
+function showClock(){
+    mainContainer.classList.add("hide");
+    clockSound.play();
+};
+
+clockDiv.addEventListener('animationend', showMainContainer);
+function showMainContainer(){
+    mainContainer.classList.remove("hide");
+    clockDiv.classList.add("hide");
+};
+
+
